19f836317452e56979f8b0b831558193be13d6b0
Ali Hussain Arafat <alihma188@gmail.com>
2025-11-04 15:48:17 +0300

    first commit

---
+++ src/test/java/com/bazaarstores/pages/RegistrationPage.java
@@ -0,0 +1,14 @@
+package com.bazaarstores.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class RegistrationPage extends BasePage {
+
+
+    public RegistrationPage(WebDriver driver) {
+        super(driver);
+    }
+
+}
